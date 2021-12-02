d9feb94f390a9bc736cb40897f2815c89d3e5a1f
Dawid Szczepankowski <davidson1@o2.pl>
2021-12-02 10:43:19 +0100

    New Test files, and modified gauss

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,5 +1,5 @@
 #include "gauss.h"
-
+#include <stdio.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -8,11 +8,15 @@ int eliminate(Matrix *mat, Matrix *b){
 	if(mat->c!=mat->r)
 		return 1;
 	int n=mat->c;
+	double q;
+	if(b->r != n){
+		return 2;
+	}
 	for(int k=0;k<n-1;k++){
-		for(int w=k+1;w<n;w++){	
+		for(int w=k+1;w<n;w++){
 			if(mat->data[k][k]==0)
 				return 1;
-			double q=mat->data[w][k]/mat->data[k][k];
+		        q=mat->data[w][k]/mat->data[k][k];
 			for(int i=0;i<n;i++)
 				mat->data[w][i]-=q*mat->data[k][i];
 			b->data[w][1]-=q*b->data[k][1];
