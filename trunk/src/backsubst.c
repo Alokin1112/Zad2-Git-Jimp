9ed88704c356887bc553cf5ca9df95808abbb550
Jakub Wysocki <wysocki369@gmail.com>
2021-12-08 12:57:39 +0100

    funkcja odwrotnie podstawiająca

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -4,7 +4,26 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
+
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+        if (mat->r!=mat->c || b->r!=mat->r || b->c!=1){
+                return 2;
+        }
+        int w;
+        int k;
+        for (w = 0; w < mat->r ; w++){
+                if (mat->data[w][w] == 0 ){
+                        return 1;
+                }
+        }
+        double sum;
+        for (w = mat->r -1; w>=0 ; w--){
+                sum = 0;
+                for (k = w + 1; k < mat->r; k++){
+                        sum += mat->data[w][k] * b->data[k][0];
+                }
+                b->data[w][0] = (b->data[w][0] - sum)/mat->data[w][w];
+
 				/**
 				 * Tutaj należy umieścić właściwą implemntację.
 				 */
